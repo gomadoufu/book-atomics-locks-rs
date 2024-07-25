4f17ac3a162e8f0cad247082980abba1a5117635
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-07-25 12:30:24 +0900

    joinして待つとうまくいく

--- src/bin/thread-spawn.rs
+++ src/bin/thread-spawn.rs
@@ -1,10 +1,13 @@
 use std::thread;
 
 fn main() {
-    thread::spawn(f);
-    thread::spawn(f);
+    let t1: thread::JoinHandle<()> = thread::spawn(f);
+    let t2: thread::JoinHandle<()> = thread::spawn(f);
 
     println!("Hello from the main thread!");
+
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
 
 fn f() {
